b93b204a9e6da525d559e2f1292d0a05342f4ed0
leehyewon <tnddl2345@gmail.com>
2022-10-13 21:23:57 +0900

    users make login

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,9 @@ app_name = 'users'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('user/', views.user, name='user'),
+
+    # 변수명 url 사용
+    path('<str:username>/', views.profile, name='profile'),
 ]
